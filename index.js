275b25403f4fc46e480dec89d48b5658b1b0f096
natusm1891 <natuss.m@gmail.com>
2022-08-23 14:14:37 -0300

    Adicionado monitor de IP

--- index.js
+++ index.js
@@ -7,11 +7,3 @@ const PORT = process.env.PORT || 3000
 const HOST = '127.0.0.1'
 
 const servidor = http.createServer(handler).listen(PORT, HOST, () => console.log(`Server is running at ${PORT}`))
-    //  (req, res) => {
-//     res.writeHead(200,{'Content-Type':'text/html'})
-//     const p = url.parse(req.url, true).query
-//     const resultado = roll(parseInt(p.valor))
-//     res.write(resultado)
-//     res.end()
-// }
-// servidor
--- services/routes.js
+++ services/routes.js
@@ -1,7 +1,18 @@
 //import {once} from 'node:events'
 import url from 'node:url'
-import { HTML_HEADER, API_HEADER } from '../utils/util.js'
+//import { HTML_HEADER, API_HEADER } from '../utils/util.js'
 import { roll } from './diceRoll.js'
+
+function getIp(req){
+    const remoteIp = (req.headers['x-forwarded-for'] || '').split(',').pop() || // Recupera o IP de origem, caso a fonte esteja utilizando proxy
+    req.connection.remoteAddress || // Recupera o endereço remoto da chamada
+    req.socket.remoteAddress || // Recupera o endereço através do socket TCP
+    req.connection.socket.remoteAddress // Recupera o endereço através do socket da conexão
+
+    console.log(remoteIp)
+    return remoteIp
+}
+
 
 
 const routes = () => ({
@@ -9,6 +20,7 @@ const routes = () => ({
     const p = await url.parse(req.url, true).query
     const resultado = await roll(parseInt(p.valor))
     res.write(JSON.stringify(resultado))
+    getIp(req)
     return res.end()
     },
     '/api:post': async (request, response) =>{
